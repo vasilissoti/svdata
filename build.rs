87eab5466f2ddf9a7828cb61136f2aeff53d0e9c
vasilissoti <vss119@ic.ac.uk>
2022-05-18 13:21:51 +0200

    (testing-yaml): Adding build.rs
    Adding build.rs and the required directories for enabling automatic test creation.

---
+++ build.rs
@@ -0,0 +1,32 @@
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
+use walkdir::WalkDir;
+
+fn main() {
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    let mut entries = Vec::new();
+    for entry in WalkDir::new("testcases/sv_files") {
+        let entry = entry.unwrap();
+        if entry.file_type().is_file() {
+            let file_name = String::from(entry.path().file_stem().unwrap().to_string_lossy());
+            entries.push(file_name);
+        }
+    }
+
+    // -------------------------------------------------------------------------------------------------
+    // Output 'test.rs'
+    // -------------------------------------------------------------------------------------------------
+
+    let out_test = Path::new(&out_dir).join("tests.rs");
+    let mut out_test = File::create(&out_test).unwrap();
+
+    for file_name in &entries {
+        let _ = write!(out_test, "#[test]\n");
+        let _ = write!(out_test, "fn test_{}() {{\n", file_name);
+        let _ = write!(out_test, "    tests(\"{}\");\n", file_name);
+        let _ = writeln!(out_test, "}}\n");
+    }
+}
