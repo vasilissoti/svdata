676ad489ec384f511363d572d9dcff33829eae9c
vasilissoti <vss119@ic.ac.uk>
2022-05-20 11:55:16 +0200

    (testing-yaml): Adding testcases

---
+++ testcases/sv/tst1.sv
@@ -0,0 +1,6 @@
+module Doo
+( input var string tmp
+, input int signed a, logic b
+, output wire signed c, unsigned d
+);
+endmodule
---
+++ testcases/sv/tst2.sv
@@ -0,0 +1,7 @@
+module Boo
+( input tri a
+, input reg b
+, output signed c, d
+, output logic signed e
+);
+endmodule
