dd85732a97edabe2b3e8b588f1fb20632aa14c66
vasilissoti <vss119@ic.ac.uk>
2022-05-23 17:02:10 +0200

    (test-configuration): Enabling parsing from settings.txt

--- build.rs
+++ build.rs
@@ -1,6 +1,7 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs::File;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::path::Path;
 use walkdir::WalkDir;
 
@@ -19,6 +20,93 @@ fn main() {
     // -------------------------------------------------------------------------------------------------
     // Output 'test.rs'
     // -------------------------------------------------------------------------------------------------
+
+    let mut overall_excluded_files: Vec<String> = Vec::new();
+    let mut custom_excluded_files_display: Vec<String> = Vec::new();
+    let mut custom_excluded_files_json: Vec<String> = Vec::new();
+    let mut custom_excluded_files_yaml: Vec<String> = Vec::new();
+    let mut format_settings: HashMap<String, String> = HashMap::new(); // VNotes: keeping the test settings (formats of data tested)
+
+    let settings_file = File::open("testcases/settings.txt");
+    let settings_file = BufReader::new(settings_file.unwrap());
+
+    let mut begin_overall_excluding: bool = false;
+    let mut begin_display_excluding: bool = false;
+    let mut begin_json_excluding: bool = false;
+    let mut begin_yaml_excluding: bool = false;
+
+    let mut set_display_format: bool = false;
+    let mut set_json_format: bool = false;
+    let mut set_yaml_format: bool = false;
+
+    for line in settings_file.lines() {
+        for word in line.unwrap().split_whitespace() {
+            match word {
+                "[Files_Excluded_Overall_Testing:]" => {
+                    begin_overall_excluding = true;
+                    continue;
+                }
+
+                "[Tested_Formats_To_Run:]" => {
+                    begin_overall_excluding = false;
+                    continue;
+                }
+
+                "Display:" => {
+                    set_display_format = true;
+                    continue;
+                }
+
+                "JSON:" => {
+                    set_display_format = false;
+                    set_json_format = true;
+                    continue;
+                }
+
+                "YAML:" => {
+                    set_json_format = false;
+                    set_yaml_format = true;
+                    continue;
+                }
+
+                "[Files_Excluded_Display:]" => {
+                    set_yaml_format = false;
+                    begin_display_excluding = true;
+                    continue;
+                }
+
+                "[Files_Excluded_JSON:]" => {
+                    begin_display_excluding = false;
+                    begin_json_excluding = true;
+                    continue;
+                }
+
+                "[Files_Excluded_YAML:]" => {
+                    begin_json_excluding = false;
+                    begin_yaml_excluding = true;
+                    continue;
+                }
+
+                _ => {}
+            };
+
+            if begin_overall_excluding {
+                overall_excluded_files.push(String::from(word));
+            } else if set_display_format {
+                format_settings.insert(String::from("Display"), String::from(word));
+            } else if set_json_format {
+                format_settings.insert(String::from("JSON"), String::from(word));
+            } else if set_yaml_format {
+                format_settings.insert(String::from("YAML"), String::from(word));
+            } else if begin_display_excluding {
+                custom_excluded_files_display.push(String::from(word));
+            } else if begin_json_excluding {
+                custom_excluded_files_json.push(String::from(word));
+            } else if begin_yaml_excluding {
+                custom_excluded_files_yaml.push(String::from(word));
+            }
+        }
+    }
 
     let t = Path::new(&out_dir).join("tests.rs");
     let mut t = File::create(&t).unwrap();
