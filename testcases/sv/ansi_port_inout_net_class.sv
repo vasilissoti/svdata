2aa96d606ae4e1a131f4c2de2498656cadad12a9
vasilissoti <vss119@ic.ac.uk>
2022-05-24 15:30:56 +0200

    (adding-testcases): Removing struct/enum definitions

--- testcases/sv/ansi_port_inout_net_class.sv
+++ testcases/sv/ansi_port_inout_net_class.sv
@@ -1,12 +1,3 @@
-struct {
-    logic [FOO-1:0] bar;
-} my_struct_t;
-
-enum bit [1:0]
-{
-  a, b, c
-} my_enum_t;
-
 module TestModule
 ( inout my_enum_t d
 , inout wire my_struct_t e, f
--- testcases/sv/ansi_port_output_var_class.sv
+++ testcases/sv/ansi_port_output_var_class.sv
@@ -1,12 +1,3 @@
-struct {
-    logic [FOO-1:0] bar;
-} my_struct_t;
-
-enum bit [1:0]
-{
-  a, b, c
-} my_enum_t;
-
 module TestModule
 ( output my_enum_t d
 , output var my_struct_t e, f
