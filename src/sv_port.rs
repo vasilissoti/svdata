9d3923c4c1545eb70cc68ea94873d64056d47b58
vasilissoti <vss119@ic.ac.uk>
2022-06-10 15:34:01 +0200

    (port-parametersdims-ansi): Adding implementation for packed/unpacked dims

--- src/sv_port.rs
+++ src/sv_port.rs
@@ -22,8 +22,11 @@ pub fn port_declaration_ansi(
             datatype: port_datatype_ansi(p, syntax_tree),
             classid: port_classid_ansi(p, &port_datatype_ansi(p, syntax_tree), syntax_tree),
             signedness: port_signedness_ansi(p, &port_datatype_ansi(p, syntax_tree)),
-            packed_dimensions: port_packeddim_ansi(p, syntax_tree),
-            unpacked_dimensions: port_unpackeddim_ansi(p, syntax_tree),
+            packed_dimensions: port_packeddim_ansi(RefNode::AnsiPortDeclaration(p), syntax_tree),
+            unpacked_dimensions: port_unpackeddim_ansi(
+                RefNode::AnsiPortDeclaration(p),
+                syntax_tree,
+            ),
         }
     } else {
         ret = SvPort {
@@ -35,7 +38,10 @@ pub fn port_declaration_ansi(
             classid: prev_port.clone().unwrap().classid,
             signedness: prev_port.clone().unwrap().signedness,
             packed_dimensions: prev_port.clone().unwrap().packed_dimensions,
-            unpacked_dimensions: port_unpackeddim_ansi(p, syntax_tree),
+            unpacked_dimensions: port_unpackeddim_ansi(
+                RefNode::AnsiPortDeclaration(p),
+                syntax_tree,
+            ),
         };
     }
 
@@ -63,6 +69,8 @@ pub fn port_parameter_declaration_ansi(
         classid: port_parameter_classid_ansi(common_data.clone(), &param_datatype, syntax_tree),
         signedness: param_signedness,
         signedness_status: param_signedness_status,
+        packed_dimensions: port_packeddim_ansi(common_data, syntax_tree),
+        unpacked_dimensions: port_unpackeddim_ansi(RefNode::ParamAssignment(p), syntax_tree),
     }
 }
 
@@ -452,10 +460,7 @@ fn port_signedness_ansi(
     }
 }
 
-fn port_packeddim_ansi(
-    m: &sv_parser::AnsiPortDeclaration,
-    syntax_tree: &SyntaxTree,
-) -> Vec<SvPackedDimension> {
+fn port_packeddim_ansi(m: RefNode, syntax_tree: &SyntaxTree) -> Vec<SvPackedDimension> {
     let mut ret: Vec<SvPackedDimension> = Vec::new();
 
     for node in m {
@@ -484,10 +489,7 @@ fn port_packeddim_ansi(
     ret
 }
 
-fn port_unpackeddim_ansi(
-    m: &sv_parser::AnsiPortDeclaration,
-    syntax_tree: &SyntaxTree,
-) -> Vec<SvUnpackedDimension> {
+fn port_unpackeddim_ansi(m: RefNode, syntax_tree: &SyntaxTree) -> Vec<SvUnpackedDimension> {
     let mut ret: Vec<SvUnpackedDimension> = Vec::new();
 
     for node in m {
@@ -497,10 +499,9 @@ fn port_unpackeddim_ansi(
                 match range {
                     Some(RefNode::ConstantRange(sv_parser::ConstantRange { nodes })) => {
                         let (l, _, r) = nodes;
-                        let left =
-                            get_string(RefNode::ConstantExpression(&l), syntax_tree).unwrap();
+                        let left = get_string(RefNode::ConstantExpression(l), syntax_tree).unwrap();
                         let right =
-                            get_string(RefNode::ConstantExpression(&r), syntax_tree).unwrap();
+                            get_string(RefNode::ConstantExpression(r), syntax_tree).unwrap();
 
                         ret.push((left.clone(), Some(right.clone())));
                     }
