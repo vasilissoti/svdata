7aa4030fa653464428e40c7da35f605ca36abd23
vasilissoti <vss119@ic.ac.uk>
2022-06-20 11:43:34 +0200

    (expression-resolver): Adding testcases for resolver

--- testcases/sv/ansi_port_parameter_keyword.sv
+++ testcases/sv/ansi_port_parameter_keyword.sv
@@ -2,6 +2,7 @@ module TestModule
 #(parameter a = 8'sb10101010,
   parameter logic signed [3:0] b = 4'b1000, c [4:0],
   parameter my_struct d,
-  parameter e = 7.5, f = 5)
+  parameter e = 7.5, f = 5,
+  parameter g = "hello")
 (); 
 endmodule
--- testcases/sv/ansi_port_parameter_nokeyword.sv
+++ testcases/sv/ansi_port_parameter_nokeyword.sv
@@ -2,6 +2,7 @@ module TestModule
 #(integer a = 8'sb10101010, 
   logic signed [3:0] b = 4'b1000, c [4:0],
   real e = 7.5, 
-  integer f = 5)
+  integer f = 5,
+  string g = "hello")
 (); 
 endmodule
