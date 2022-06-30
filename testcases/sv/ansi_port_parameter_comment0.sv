2e25b0cfd90db5b7f5247190cf5f908d997e9ed2
vasilissoti <vss119@ic.ac.uk>
2022-06-30 18:59:36 +0200

    (parameter-comment): Adding testcases

---
+++ testcases/sv/ansi_port_parameter_comment0.sv
@@ -0,0 +1,5 @@
+module TestModule
+#(localparam a = (8'sb10101010 + "hello") /* A0 */, b = 3, // A1
+parameter /* B0 */ c = 1 /* B1 */, d = 6, e)
+(); 
+endmodule
---
+++ testcases/sv/ansi_port_parameter_comment1.sv
@@ -0,0 +1,5 @@
+module TestModule
+#(integer a = (8'sb10101010 + "hello") /* A0 */, b = 3, // A1
+/* B0 */ c = 1 /* B1 */, d = 6, e)
+(); 
+endmodule
