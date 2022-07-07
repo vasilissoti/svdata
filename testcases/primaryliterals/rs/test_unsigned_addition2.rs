f6c3bbee5c02f703d064bd90e37f82ef1bea5f1b
vasilissoti <vss119@ic.ac.uk>
2022-07-07 11:40:51 +0200

    (resolver-range): Adding testcases

--- testcases/primaryliterals/rs/test_unsigned_addition2.rs
+++ testcases/primaryliterals/rs/test_unsigned_addition2.rs
@@ -1,9 +1,9 @@
 let mut a = SvPrimaryLiteral {
-    data01: vec![9223372036854775808],
-    num_bits: 64,
+    data01: vec![4611686018427387904],
+    num_bits: 63,
     signed: false,
 };
 
-a.usize_add(9223372036854775808);
+a.usize_add(4611686018427387904);
 
 let actual_string = format!("{}", a);
--- testcases/primaryliterals/rs/test_unsigned_addition4.rs
+++ testcases/primaryliterals/rs/test_unsigned_addition4.rs
@@ -1,6 +1,6 @@
 let mut a = SvPrimaryLiteral {
-    data01: vec![9223372036854775808],
-    num_bits: 64,
+    data01: vec![9223372036854775808, 9223372036854775808],
+    num_bits: 128,
     signed: false,
 };
 
