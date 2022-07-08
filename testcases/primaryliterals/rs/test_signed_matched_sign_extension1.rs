8f107acbefe419d3dc720457d096bff90268397a
vasilissoti <vss119@ic.ac.uk>
2022-07-08 14:48:59 +0200

    (tesing-svprimlit): Adding test cases for "_signed_matched_sign_extension"

---
+++ testcases/primaryliterals/rs/test_signed_matched_sign_extension1.rs
@@ -0,0 +1,15 @@
+let mut a = SvPrimaryLiteral {
+    data01: vec![9223372036854775808, 9223372036854775808],
+    num_bits: 128,
+    signed: true,
+};
+
+let mut b = SvPrimaryLiteral {
+    data01: vec![0, 9223372036854775808],
+    num_bits: 65,
+    signed: true,
+};
+
+a._signed_matched_sign_extension(&mut b);
+
+let actual_string = format!("{}", a);
