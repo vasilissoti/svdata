c668cb4b95f7220979088a1b5da6889df825fd39
vasilissoti <vss119@ic.ac.uk>
2022-07-14 15:19:15 +0200

    (svprimlit-methods): Adding test cases for eq

---
+++ testcases/primaryliterals/rs/eq3.rs
@@ -0,0 +1,15 @@
+let a = SvPrimaryLiteral {
+    data01: vec![4611686018427387904],
+    num_bits: 64,
+    signed: true,
+};
+
+let b = SvPrimaryLiteral {
+    data01: vec![0, 9223372036854775808],
+    num_bits: 65,
+    signed: true,
+};
+
+let c: bool = a.eq(b.clone());
+
+let actual_string = format!("{}", c);
