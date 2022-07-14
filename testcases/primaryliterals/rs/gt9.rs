7ad02f5b9a4e953fc2b7ad7bc0a7f4093b08d998
vasilissoti <vss119@ic.ac.uk>
2022-07-14 15:06:11 +0200

    (svprimlit-methods): Bugfix + Test cases for gt

---
+++ testcases/primaryliterals/rs/gt9.rs
@@ -0,0 +1,15 @@
+let a = SvPrimaryLiteral {
+    data01: vec![0, 9223372036854775808],
+    num_bits: 65,
+    signed: true,
+};
+
+let b = SvPrimaryLiteral {
+    data01: vec![0, 9223372036854775808],
+    num_bits: 66,
+    signed: true,
+};
+
+let c: bool = a.gt(b.clone());
+
+let actual_string = format!("{}", c);
