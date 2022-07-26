46e38a0ed3b4fa0177d60a55cfaa6aec96e9c5de
vasilissoti <vss119@ic.ac.uk>
2022-07-26 17:14:13 +0200

    (svprimlit-integral): Adding test cases for signed 4-state addition

---
+++ testcases/primaryliterals/integral/rs/4state_signed_addition_pos_pos3.rs
@@ -0,0 +1,26 @@
+let a = SvPrimaryLiteralIntegral {
+    data_01: vec![4611686018427387904, 4611686018427387904],
+    data_xz: Some(vec![0, 0]),
+    size: 128,
+    signed: true,
+};
+
+let b = SvPrimaryLiteralIntegral {
+    data_01: vec![4611686018427387904],
+    data_xz: Some(vec![0]),
+    size: 64,
+    signed: true,
+};
+
+let c: SvPrimaryLiteralIntegral = a.add_primlit(b.clone());
+
+let exp = SvPrimaryLiteralIntegral {
+    data_01: vec![4611686018427387904, 9223372036854775808],
+    data_xz: Some(vec![0, 0]),
+    size: 128,
+    signed: true,
+};
+
+assert_eq!(c, exp);
+
+let actual_string = format!("{}", c);
