5058b161b88d4e50549500a6c8bccfe2c97800c5
vasilissoti <vss119@ic.ac.uk>
2022-07-28 12:31:23 +0200

    (svprimlit-integral): Added 4-state (X/Z/non) test cases for rol

---
+++ testcases/primaryliterals/integral/rs/4state_xz_rol1.rs
@@ -0,0 +1,19 @@
+let a = SvPrimaryLiteralIntegral {
+    data_01: vec![9223372036854775808, 9223372036854775808],
+    data_xz: Some(vec![0, 9223372036854775808]),
+    size: 128,
+    signed: true,
+};
+
+let b: SvPrimaryLiteralIntegral = a.rol(2);
+
+let exp = SvPrimaryLiteralIntegral {
+    data_01: vec![2, 2],
+    data_xz: Some(vec![2, 0]),
+    size: 128,
+    signed: true,
+};
+
+assert_eq!(b, exp);
+
+let actual_string = format!("{}", b);
