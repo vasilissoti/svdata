d9da32cc9bcf1168af6d62b3a0f3fa0753448410
vasilissoti <vss119@ic.ac.uk>
2022-07-29 18:00:39 +0100

    (svprimlit-cmp): Adding test cases for cmp between signed & unsigned

---
+++ testcases/primaryliterals/integral/rs/2state_lt11.rs
@@ -0,0 +1,19 @@
+let a = SvPrimaryLiteralIntegral {
+    data_01: vec![0, 9223372036854775808],
+    data_xz: None,
+    size: 65,
+    signed: true,
+};
+
+let b = SvPrimaryLiteralIntegral {
+    data_01: vec![9223372036854775808],
+    data_xz: None,
+    size: 66,
+    signed: false,
+};
+
+let c: bool = a < b;
+
+assert_eq!(c, false);
+
+let actual_string = format!("{}", c);
---
+++ testcases/primaryliterals/integral/rs/2state_lt12.rs
@@ -0,0 +1,19 @@
+let a = SvPrimaryLiteralIntegral {
+    data_01: vec![9223372036854775808],
+    data_xz: None,
+    size: 64,
+    signed: true,
+};
+
+let b = SvPrimaryLiteralIntegral {
+    data_01: vec![9223372036854775808],
+    data_xz: None,
+    size: 64,
+    signed: false,
+};
+
+let c: bool = a < b;
+
+assert_eq!(c, true);
+
+let actual_string = format!("{}", c);
