6a086ce5460bc1f982989ef543e028241599d6fa
sirirust <siri.rusten@nordicsemi.no>
2022-08-12 13:14:27 +0200

    Crating and fixing testcases

--- testcases/semantics/sv/instantiation_byname_multiple_modules.sv
+++ testcases/semantics/sv/instantiation_byname_multiple_modules.sv
@@ -1,16 +1,28 @@
-module mkM1 (a);
-    
+module mkM1 (
+        input a_in, 
+        output a_out
+    );
 endmodule
 
-module mkM2 (b);
-    mkM1 m1 (.b0(b)); // instantiates mkM1
+module mkM2 (
+        input b_in, 
+        output b_out
+    );
+    mkM1 m1 (.a_in(b_in), .a_out(b_out)); // instantiates mkM1
 endmodule
 
-module mkM3 (c);
-    mkM1 m1 (.c0(c)); // instantiates mkM1
-    mkM2 m2 (.c0(c)); // instantiates mkM2
+module mkM3 (
+        input c_in, 
+        output c_out
+    );
+    wire w;
+    mkM1 m1 (.a_in(c_in), .a_out(w)); // instantiates mkM1
+    mkM2 m2 (.b_in(w), .b_out(c_out)); // instantiates mkM2
 endmodule
 
-module mkM4 (d);
-    mkM3 m3 (.d0(d));  // instantiates mkM3
+module mkM4 (
+        input d_in, 
+        output d_out
+    );
+    mkM3 m3 (.c_in(d_in), .c_out(d_out));  // instantiates mkM3
 endmodule
