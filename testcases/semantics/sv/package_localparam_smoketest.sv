af9a3023a21cba5908e2e03d7ccf9975077426a5
damc <dave.mcewan@nordicsemi.no>
2022-08-30 17:33:20 +0200

    unwrap-bugfix Rename package_localparam -> package_smoketest

--- testcases/semantics/sv/package_localparam_smoketest.sv
+++ testcases/semantics/sv/package_localparam_smoketest.sv
@@ -1,6 +1,7 @@
-package TestPackage;
+package P_smoketest;
   localparam a = 8'sb10101010;
   localparam logic signed [3:0] b = 4'b1000;
   localparam e = 7.5, f = 5;
   localparam g = "hello";
+  localparam event foo;
 endpackage
