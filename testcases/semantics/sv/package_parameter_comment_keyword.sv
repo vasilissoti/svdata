0ce1866df14d624dd27fca18890a7badd6f7c462
damc <dave.mcewan@nordicsemi.no>
2022-08-30 15:51:20 +0200

    unwrap-bugfix Remove replaced testcases.

--- testcases/semantics/sv/package_parameter_comment_keyword.sv
+++
@@ -1,8 +0,0 @@
-package TestPackage;
-  /*pre*/ parameter /*kw/ty*/ a /*post*/ = /*eq/val*/ 1 /* post */;
-  /*pre*/ parameter /*kw/ty*/ b /* id/eq */ = /*eq/val*/ 5 /* post */;
-  /*pre*/ parameter /*kw/ty*/ int /*ty/id*/ c /*post*/ = /*eq/val*/ 3 /* post */;
-  /*pre*/ parameter /*kw/ty*/ int /*ty/id*/ d /*id/eq*/ = /*eq/val*/ 2 /*post*/;
-  /*pre*/ localparam /*kw/ty*/ e /* id/eq */ = /*eq/val*/ 5 /* post */;
-  /*pre*/ localparam /*kw/ty*/ int /*ty/id*/ f /*id/eq*/ = /*eq/val*/ 5 /*post*/;
-endpackage
