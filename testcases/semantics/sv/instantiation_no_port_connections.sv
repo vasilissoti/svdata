538b0ca3031500600ef112f92ac93024f743aa4e
vasilissoti <vss119@ic.ac.uk>
2022-09-13 18:15:36 +0200

    (bugfix-instance): Test case addition

--- testcases/semantics/sv/instantiation_no_port_connections.sv
+++ testcases/semantics/sv/instantiation_no_port_connections.sv
@@ -1,3 +1,7 @@
-module M #()();
-  I #(.A(A)) u_I [B-1:0] ();
+module M1 #()();
+  I1 #(.A(A)) u_I1 [B-1:0] (); // Empty ordered port connection
+endmodule
+
+module M2 #()();
+  I2 #(.A(A)) u_I2 [B-1:0] (.C()); // Empty named port connection
 endmodule
